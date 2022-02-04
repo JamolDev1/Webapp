659fc2ade2c35f8639a3f92fe03d0dadc82fd44e
muydinjanovv <muhammadjonmuydinjanov03@gmail.com>
2022-02-04 18:52:00 +0500

    Created Organizations relationships

--- webapp/Data/AppDbContext.cs
+++ webapp/Data/AppDbContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using webapp.Entity;
 
-namespace invoice.Data;
+namespace webapp.Data;
 public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions options)
         : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>(au => 
+        {
+            au.HasMany(u => u.Organizations)
+                .WithOne(i => i.Owner)
+                .HasForeignKey(i => i.OwnerId)
+                .OnDelete(DeleteBehavior.SetNull);
+        });
+    }
 }
